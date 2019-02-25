d69eb7635f02b3f187d1c593995d64d4346b20d6
druiz <druiz0992@gmail.com>
2019-02-25 22:34:03 +0100

    Working on fft

--- src/cuda/zpoly_device.cu
+++ src/cuda/zpoly_device.cu
@@ -59,16 +59,11 @@ __global__ void fft_kernel(uint32_t *out_vector, uint32_t *in_vector, kernel_par
     
     if (params->premod){
       #pragma unroll
-      for (i=0; i< params->stride/2-1; i++){
+      for (i=0; i< params->stride; i++){
         modu256(&x[i*NWORDS_256BIT],&x[i*NWORDS_256BIT], params->midx);
-        modu256(&y[i*NWORDS_256BIT],&y[i*NWORDS_256BIT], params->midx);
       }
     }
-
-   #pragma unroll
-   for (i=0; i< params->stride/2-1; i++){
-      addmu256(&z[i*NWORDS_256BIT],(const uint32_t *)&x[i*NWORDS_256BIT], (const uint32_t *)&y[i*NWORDS_256BIT], params->midx);
-   }   
+    fft(x);
 }
 
 __global__ void ifft_kernel(uint32_t *out_vector, uint32_t *in_vector, kernel_params_t *params);
@@ -77,29 +72,47 @@ __global__ void ifft_kernel(uint32_t *out_vector, uint32_t *in_vector, kernel_pa
     uint32_t i;
 
     uint32_t __restrict__ *x;
-    uint32_t __restrict__ *y;
     uint32_t __restrict__ *z;
    
     if(tid >= params->in_length/params->stride) {
       return;
     }
 
-    x = (uint32_t *) &in_vector[tid * params->stride * U256K_OFFSET + U256_XOFFSET * params->stride/2];
-    y = (uint32_t *) &in_vector[tid * params->stride * U256K_OFFSET + U256_YOFFSET * params->stride/2];
-    z = (uint32_t *) &out_vector[tid * params->stride/2 * U256K_OFFSET];
+    x = (uint32_t *) &in_vector[tid * params->stride * U256K_OFFSET];
+    z = (uint32_t *) &out_vector[tid * params->stride * U256K_OFFSET];
     
     if (params->premod){
       #pragma unroll
-      for (i=0; i< params->stride/2-1; i++){
+      for (i=0; i< params->stride; i++){
         modu256(&x[i*NWORDS_256BIT],&x[i*NWORDS_256BIT], params->midx);
-        modu256(&y[i*NWORDS_256BIT],&y[i*NWORDS_256BIT], params->midx);
       }
     }
-
-   #pragma unroll
-   for (i=0; i< params->stride/2-1; i++){
-      addmu256(&z[i*NWORDS_256BIT],(const uint32_t *)&x[i*NWORDS_256BIT], (const uint32_t *)&y[i*NWORDS_256BIT], params->midx);
-   }   
+    ifft(x);
 }
 
+__device__ void fft(uint32_t *x)
+{
+  uint32_t i, j, k;
+  uint32_t s, t;
+  uint32_t val[]={0,0,0,0,0,0,0,0};
 
+  memcpy(val,x,sizeof(uint32_t)*NWORDS_256BIT);
+
+  val += __shfl_xor_sync(0xffffffff, val[0], 1);
+  val += __shfl_xor_sync(0xffffffff, val[1], 1);
+  val += __shfl_xor_sync(0xffffffff, val[2], 1);
+  val += __shfl_xor_sync(0xffffffff, val[3], 1);
+  val += __shfl_xor_sync(0xffffffff, val[4], 1);
+  val += __shfl_xor_sync(0xffffffff, val[5], 1);
+  val += __shfl_xor_sync(0xffffffff, val[6], 1);
+  val += __shfl_xor_sync(0xffffffff, val[7], 1);
+
+  val += __shfl_xor_sync(0xffffffff, val, 2);
+  val += __shfl_xor_sync(0xffffffff, val, 4);
+  val += __shfl_xor_sync(0xffffffff, val, 8);
+  val += __shfl_xor_sync(0xffffffff, val, 16);
+
+        }
+     }
+  }
+}
