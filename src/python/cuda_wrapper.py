fixes B2 calculation

src/python/cuda_wrapper.py
@@ -285,7 +285,7 @@ def ec_mad_cuda2(pysnark, vector, fidx, ec2=False, shamir_en=0, gpu_id=0, stream
 
    return vector, result, t
 
-def ec_pippen_mul(pysnark, vector, fidx, pippen_binsize=8 , pippen_blocksize=8 , ec2=False, gpu_id=0, stream_id = 0, first_time=0):
+def ec_pippen_mul(pysnark, vector, fidx, pippen_binsize=8 , pippen_blocksize=8 , ec2=False, gpu_id=0, stream_id = 0, first_time=0, offset=0):
    kernel_params={}
    kernel_config={}
    
@@ -299,6 +299,7 @@ def ec_pippen_mul(pysnark, vector, fidx, pippen_binsize=8 , pippen_blocksize=8 ,
       kernel = [CB_EC_JAC_MUL_PIPPEN]
 
  
+   #nsamples = int(len(vector)+indims_e)
    nsamples = int(len(vector)/indims_e)
    nkernels = 1
    nblocks = 16
@@ -311,6 +312,7 @@ def ec_pippen_mul(pysnark, vector, fidx, pippen_binsize=8 , pippen_blocksize=8 ,
    kernel_params['premul']    = [first_time]
    kernel_params['premod'] = [nblocks]
    kernel_params['padding_idx'] = [pippen_binsize] 
+   kernel_params['forward'] = [offset]
    kernel_params['midx']      = [fidx] 
 
    kernel_config['kernel_idx'] = kernel
@@ -339,7 +341,6 @@ def ec_pippen_reduce(pysnark, vector, fidx, pippen_binsize=8 , pippen_blocksize=
       kernel = [CB_EC_JAC_RED1_PIPPEN, CB_EC_JAC_RED2_PIPPEN, CB_EC_JAC_RED3_PIPPEN]
 
  
-   nsamples = int(len(vector)/indims_e)
    nkernels = 3
    nblocks = 16
    nbins =int((NWORDS_FR * NBITS_WORD) / pippen_binsize) 
@@ -358,6 +359,7 @@ def ec_pippen_reduce(pysnark, vector, fidx, pippen_binsize=8 , pippen_blocksize=
    kernel_config['smemS'] = [0] * nkernels
    kernel_config['smemS'][nkernels-1] = (1<<(pippen_blocksize-5)) * NWORDS_FP * outdims * 4
    kernel_config['gridD'] = [nblocks,1,1] 
+   kernel_config['input_val'] = [0,0,0] 
 
    #writeU256DataFile_h(np.reshape(vector,-1), "/home/edu/david/cusnarks/pippen.bin".encode("UTF-8"))
    result,t = pysnark.kernelLaunch(vector, kernel_config, kernel_params, gpu_id, stream_id, n_kernels=nkernels )

src/python/groth_prover.py
@@ -1546,27 +1546,6 @@ class GrothProver(object):
           stream_id = p[4]
           cuda_ec128.streamSync(gpu_id,stream_id)
 
-    def getECResults(self, dispatch_table):
-       for bidx,p in enumerate(dispatch_table):
-          P = p[0]
-          cuda_ec128 = self.ec_type_dict[P][0]
-          step = self.ec_type_dict[P][1]
-          pidx = self.ec_type_dict[P][4]
-          gpu_id = p[3]
-          stream_id = p[4]
-          result, t = cuda_ec128.streamSync(gpu_id,stream_id)
-          if step==4:
-             self.init_ec_val[gpu_id][stream_id][pidx] =\
-                            ec2_jac2aff_h(
-                             result.reshape(-1),
-                             MOD_FP,
-                             strip_last=1) 
-          else:
-             self.init_ec_val[gpu_id][stream_id][pidx] =\
-                        ec_jac2aff_h(
-                              result.reshape(-1),
-                              MOD_FP,
-                              strip_last=1) 
 
     def init_EC_P(self, batch_size):
        nsamples = np.product(get_shfl_blockD(batch_size))
@@ -1620,12 +1599,18 @@ class GrothProver(object):
           batch[nsamples+indims*offset:] = np.reshape(
                                             ecp_vector[start_idx*NWORDS_FP*indims:end_idx*NWORDS_FP*indims],
                                             (-1,NWORDS_FP))
+          """ 
+          print(offset, nsamples)
+          batch = np.concatenate((scl_vector[start_idx:end_idx],
+                                           np.reshape( ecp_vector[start_idx*NWORDS_FP*indims:end_idx*NWORDS_FP*indims],(-1,8))))
+          """                                   
+          
 
           first_time = 1
           if stream_id in used_streams[gpu_id] :
             first_time = 0
 
-          ec_pippen_mul(cuda_ec128, batch ,MOD_FP, ec2=ec2, gpu_id=gpu_id, stream_id=stream_id, first_time=first_time)
+          ec_pippen_mul(cuda_ec128, batch ,MOD_FP, ec2=ec2, gpu_id=gpu_id, stream_id=stream_id, first_time=first_time, offset=0)
           used_streams[gpu_id].add(stream_id)
 
           if stream_id == 0: