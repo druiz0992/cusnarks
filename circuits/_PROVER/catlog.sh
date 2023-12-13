da5d6cef10591d16ecfb3fbad86a11319e97313e
David Ruiz <druiz0992@gmail.com>
2023-12-13 08:59:47 +0100

    test

--- circuits/_PROVER/catlog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-LOGFILE="$(ls -lrt log_* | tail -n 1 | rev | cut -d' ' -f1 | rev)"
-cat ${LOGFILE}
--- circuits/_PROVER/taillog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-LOGFILE="$(ls -lrt log_* | tail -n 1 | rev | cut -d' ' -f1 | rev)"
-echo $LOGFILE
-tail -f ${LOGFILE}
